0faf37fd2cf72561220e5fd9a28c655ec8ed6095
Juhi K <jkesarwani.us@gmail.com>
2023-06-11 20:04:52 -0400

    Shared Library - roboshop

---
+++ vars/terraform.groovy
@@ -0,0 +1,31 @@
+def call(int buildNumber) {
+    pipeline {
+
+        agent {
+            node {
+                label 'workspace'
+            }
+        }
+        parameters {
+            choice(name: 'env', choices: ['dev', 'prod'], description: 'Environment')
+            choice(name: 'action', choices: ['apply', 'destroy'], description: 'Build/Destroy')
+        }
+        stages {
+            stage('Terraform INIT') {
+                steps{
+                    sh 'terraform init -backend-config=env-${env}/state.tfvars'
+                }
+            }
+            stage('Terraform Apply') {
+                steps {
+                    sh 'terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars'
+                }
+            }
+        }
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
